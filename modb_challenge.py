6bf1facc75f21998a7f21b93cacd81339f63636e
22goyery <49483199+22goyery@users.noreply.github.com>
2019-05-29 10:48:16 -0400

    Add files via upload

---
+++ modb_challenge.py
@@ -0,0 +1,33 @@
+def main():
+    for food in cookies
+    print("cookies")
+    for food in pizza
+    print("pizza")
+    for food in mac and cheese
+    print("mac and cheese")
+    for food in apples
+    print("apples")
+    for first name in Ryan
+    print("Ryan")
+    for last name in goyette
+    print("goyette")
+    for age in years
+    print( 5 * 3)
+    for age in months
+    print(17*3)
+    for age in weeks
+    print(123 *456)
+    for speech in pillow
+    print("pillow")
+    for speech in fancy
+    print("fancy")
+    for speech in whirl
+    print("whirl")
+    for speech in stadium
+    print("stadium")
+
+    main()
+
+
+
+  
